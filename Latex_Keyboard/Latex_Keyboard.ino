569dc40501057200f5dab248d461a5e1c995bb07
tinkrmind <tinkrmind@gmail.com>
2018-03-21 03:52:30 -0400

    Added modifier keys example

--- Latex_Keyboard/Latex_Keyboard.ino
+++ Latex_Keyboard/Latex_Keyboard.ino
@@ -1,7 +1,14 @@
-//Latex keypad
-//Connect 4x4 keypad to pins 4 through 11 of arduino leonardo or micro
-//Code is subject to Creative Commons Sharealike 4.0 lisence
-//Author: Amitabh Shrivastava (tinkrmind@gmail.com)
+// Latex keypad
+// Connect 4x4 keypad to pins 4 through 11 of arduino leonardo or micro
+// Code is subject to Creative Commons Sharealike 4.0 lisence
+// Author: Amitabh Shrivastava (tinkrmind@gmail.com)
+//
+// While I work on UTF-8 support, you can use modifier keys to hack your way into typing accented characters and whatnot
+// To use key combinations, leave the keycode of the corresponding button blank and then add an if statement to handle the button as demonstrated below
+//
+// More info on Keyboard library at: https://www.arduino.cc/reference/en/language/functions/usb/keyboard/
+// Info on supported key modifiers etc: https://www.arduino.cc/reference/en/language/functions/usb/keyboard/keyboardmodifiers/
+//
 
 #include <Keyboard.h>
 
@@ -11,11 +18,11 @@ int i, j, button;
 const int delay_time = 300;
 String s;
 
-String keycode[]={
-  "\\pi",    "\\theta", "\\lamda",  "\\phi",
-  "\\alpha", "\\beta",  "\\gamma",  "\\delta",
-  "\\Gamma", "\\Delta", "\\Pi",     "\\Sigma",
-  "\\sim",   "\\ge",    "\\leq",    "\\equiv"
+String keycode[]={                                  // Button numbers
+  "\\pi",    "\\theta", "\\lamda",  "\\phi",        // 1   2   3   4
+  "\\alpha", "\\beta",  "\\gamma",  "\\delta",      // 5   6   7   8
+  "\\Gamma", "\\Delta", "\\Pi",     "\\Sigma",      // 9   10  11  12
+  "\\sim",   "\\ge",    "\\leq",    ""              // 13  14  15  16
 };
 
 void setup()
@@ -32,9 +39,24 @@ void setup()
 
 void loop()
 {
-  if(button = detectKey()){
+  button = detectKey();
+  if(button != 0){
     Keyboard.print(keycode[button-1]);
     delay(delay_time);
+  }
+  // If button 16 is pressed execute the following key presses
+  if(button == 16){
+    // Press Ctrl+ALt+Delete
+    Keyboard.press( KEY_LEFT_CTRL );
+    Keyboard.press( KEY_LEFT_ALT );
+    Keyboard.press( KEY_DELETE );
+    // Now release all keys at once
+    Keyboard.releaseAll(); // NOTE: Make sure you release keys after pressing them. Otherwise you can really mess you your day!
+    
+    // Now type "you are pwned" in caps
+    Keyboard.write( KEY_CAPS_LOCK );
+    Keyboard.print("you are pwned");
+
   }
 }
 
